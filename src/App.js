df18e8aaf8156d70aac55e3d90ebfed8ff19d171
vainaweb28 <you@example.com>
2020-12-01 15:08:03 -0300

    mapeamento de titulos e imagens

--- src/App.js
+++ src/App.js
@@ -1,17 +1,103 @@
-// Libs
 import React, { Component } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
+
 
 const Container = styled.div``;
 
-class App extends Component {
+
+
+class Netflix extends Component {
+  state = {
+
+    movies: [],
+    shows: [],
+    images: []
+    
+  }
+
+  async componentDidMount(){
+
+
+    const responseMovies = await axios.get(`${process.env.REACT_APP_API}/movies`);
+    // console.log(responseMovies.data);
+
+    const filmes = responseMovies.data.map((item) => {
+      return {
+        ...item,
+        poster_path: `https://image.tmdb.org/t/p/w500${item.poster_path}`
+      };
+    });
+
+    this.setState({
+      movies: responseMovies.data
+    })
+
+    this.setState({
+      images: responseMovies.data.poster_path
+    });
+
+    // console.log(filmes);
+
+
+
+    
+    const responseShows = await axios.get(`${process.env.REACT_APP_API}/shows`);
+    // console.log(responseShows.data);
+
+    const series = responseShows.data.map((item) => {
+      return {
+        ...item,
+        poster_path: `https://image.tmdb.org/t/p/w500${item.poster_path}`
+      };
+    });
+
+    this.setState({
+      shows: responseShows.data
+    })
+
+    this.setState({
+      images: responseMovies.data.poster_path
+    });
+
+    // console.log(series);
+
+
+
+
+    
+
+  }
+
+     
+
+  
+  
+
+
   render() {
-    return (
+    return(
       <Container>
-        
+        <div>
+          {this.state.movies.map((item, index) => (
+            <div key={index}>
+              <p className="netflixTitles">{item.title}</p>
+              <img className="netflixImages" src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}/>
+            </div>
+          ))}
+        </div>
+        <div>
+          {this.state.shows.map((item, index) => (
+            <div key={index}>
+              <p className="netflixTitles">{item.original_name}</p>
+              <img className="netflixImages" src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}/>
+            </div>
+          ))}
+        </div>
+          
       </Container>
-    );
+    )
   }
 }
 
-export default App;
+export default Netflix;
